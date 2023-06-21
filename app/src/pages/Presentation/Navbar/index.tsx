9c239163a4146ab8c6da225c76421f4e4ce3275f
Ieleny <ielenyfilgueira@hotmail.com>
2023-06-20 22:00:16 -0300

    feature: Create navbar component

--- app/src/pages/Presentation/Navbar/index.tsx
+++ app/src/pages/Presentation/Navbar/index.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Layout, Menu } from "antd";
+import type { MenuProps } from "antd";
+  
+const { Header } = Layout;
 
 const Navbar: React.FC = () => {
-  return <div>HEY 'I'M HERE</div>;
-};
+  
+  const [current, setCurrent] = useState("apresentacao");
 
+  const navBarMenu: MenuProps["items"] = 
+  [
+    {
+      label: "Apresentação",
+      key: "apresentacao",
+    },
+    {
+      label: "Contato",
+      key: "contato",
+    },
+  ];
+  
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+
+  return (
+      <Header style={{ display: "flex", alignItems: "center" }}>
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={navBarMenu}
+        />
+      </Header>
+  );
+    
+}
+      
 export default Navbar;
--- app/src/pages/Presentation/index.tsx
+++ app/src/pages/Presentation/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import Navbar from './Navbar';
+import { Layout, theme } from 'antd';
 
-const { Header, Content, Footer } = Layout;
+const { Content, Footer } = Layout;
 
 const Presentation: React.FC = () => {
     const {
@@ -10,27 +11,8 @@ const Presentation: React.FC = () => {
     
     return (
       <Layout className="layout">
-      <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['5']}
-          items={new Array(10).fill(null).map((_, index) => {
-            const key = index + 1;
-            return {
-              key,
-              label: `nav ${key}`,
-            };
-          })}
-        />
-      </Header>
+      <Navbar />
       <Content style={{ padding: '0 50px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
         <div className="site-layout-content" style={{ background: colorBgContainer }}>
           Content
         </div>
