f33414b42782188d70c70cdf8e3e9fd36c5c10cc
Ieleny <ielenyfilgueira@hotmail.com>
2023-07-22 16:31:16 -0300

    feature: apply router

--- app/src/pages/root/App.tsx
+++ app/src/pages/root/App.tsx
@@ -1,30 +1,17 @@
-import { Layout, theme } from "antd";
-import Navbar from "src/components/Navbar";
-import Routes from "src/pages/Routes";
 import "./App.css";
-
-const { Content, Footer } = Layout;
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ManagementSystem from "src/pages/Layout/ManagementSystem";
+import ViewMaterialList from "src/pages/Material/View/List/MaterialListView";
 
 function App() {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-
   return (
-    <Layout className="layout">
-      <Navbar />
-      <Content style={{ padding: "0 50px" }}>
-        <div
-          className="site-layout-content"
-          style={{ background: colorBgContainer }}
-        >
-          <Routes />
-        </div>
-      </Content>
-      <Footer style={{ textAlign: "center" }}>
-        Planeje financeiramente a sua construção @2023
-      </Footer>
-    </Layout>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ManagementSystem />}>
+          <Route path="/adicionar-lista" element={<ViewMaterialList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
