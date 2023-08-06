edf8b360d5bfecb90b571b539253a4f73b940aae
Ieleny <ielenyfilgueira@hotmail.com>
2023-08-06 02:15:08 -0300

    feature: Add component in route

--- app/src/pages/root/App.tsx
+++ app/src/pages/root/App.tsx
@@ -2,16 +2,17 @@ import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import ManagementSystem from "src/pages/Layout/ManagementSystem";
 import MaterialAddListView from "src/pages/Presentation/Material/View/Add/MaterialAddView";
+import MaterialListView from "src/pages/Presentation/Material/View/List/MaterialListView";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ManagementSystem />}>
+          <Route path="apresentacao" index element={<MaterialAddListView />} />
           <Route path="adicionar-lista" element={<MaterialAddListView />} />
-          <Route path="apresentacao" element={<MaterialAddListView />} />
           <Route path="contato" element={<MaterialAddListView />} />
-          <Route path="lista" element={<MaterialAddListView />} />
+          <Route path="lista" element={<MaterialListView />} />
         </Route>
       </Routes>
     </BrowserRouter>
