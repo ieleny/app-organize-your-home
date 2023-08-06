c3d04fad2f4f4842e56a3598b943b8c46e558c42
Ieleny <ielenyfilgueira@hotmail.com>
2023-08-06 13:03:23 -0300

    fix: removed columns and array in listMaterial

--- app/src/pages/Presentation/Material/View/List/MaterialListViewModel.ts
+++ app/src/pages/Presentation/Material/View/List/MaterialListViewModel.ts
@@ -3,25 +3,7 @@ import { MaterialController } from "src/pages/Presentation/Material/Store/Materi
 export default class MaterialListViewModel {
   private materialController = new MaterialController();
 
-  public columns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-    },
-    {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
-    },
-    {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
-    },
-  ];
-
   listMaterial() {
-    return [this.materialController.listMaterial()];
+    return this.materialController.listMaterial();
   }
 }
