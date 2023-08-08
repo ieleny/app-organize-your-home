aa731012ddae6215c3249bd9cb34d4915ae48aba
Ieleny <ielenyfilgueira@hotmail.com>
2023-08-07 22:52:15 -0300

    feature: MaterialController to Data folder

---
+++ app/src/pages/Data/Store/MaterialController.ts
@@ -0,0 +1,26 @@
+import { MaterialType } from "src/types/material";
+import { IMaterial } from "src/interface/material";
+import { MateriaisAtom, store } from "./MateriasStore";
+
+// dependency inversion
+export class MaterialController {
+  
+  public addMaterial({ materiais }: MaterialType) {
+
+    const arrayMaterials: IMaterial[] = [
+      ...store.get(MateriaisAtom),
+      materiais,
+    ];
+
+    store.set(MateriaisAtom, arrayMaterials);
+  }
+
+  public listMaterial() {
+    return store.get(MateriaisAtom);
+  }
+
+  public countMaterials() {
+    return store.get(MateriaisAtom).length;
+  }
+
+}
