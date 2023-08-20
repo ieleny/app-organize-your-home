bcb65fcf82afe428f282a2e35ad1c06619b6d061
Ieleny <ielenyfilgueira@hotmail.com>
2023-08-20 18:05:09 -0300

    refactor: remove view model in pages

--- app/src/pages/Presentation/Material/View/Add/MaterialAddView.tsx
+++ app/src/pages/Presentation/Material/View/Add/MaterialAddView.tsx
@@ -2,26 +2,20 @@ import React from "react";
 import {
   Typography,
   Row,
-  Divider,
 } from "antd";
 
-import MaterialAddViewModel from "./MaterialAddViewModel";
 import MaterialForm from "../Form/MaterialForm";
 
 const { Title } = Typography;
 
 const MaterialAddListView: React.FC = () => {
-  const materialAddViewModel = new MaterialAddViewModel();
-
   return (
     <>
       <Row justify="center">
         <Title>Adicione seus gastos com a construção</Title>
       </Row>
 
-      <MaterialForm materialViewModel={materialAddViewModel} />
-
-      <Divider orientation="center"></Divider>
+      <MaterialForm />
     </>
   );
 };
--- app/src/pages/Presentation/Material/View/Edit/MaterialEditView.tsx
+++ app/src/pages/Presentation/Material/View/Edit/MaterialEditView.tsx
@@ -4,22 +4,18 @@ import {
   Row,
 } from "antd";
 
-import MaterialEditViewModel from "./MaterialEditViewModel";
 import MaterialForm from "src/pages/Presentation/Material/View/Form/MaterialForm";
 
 const { Title } = Typography;
 
 const MaterialEditListView: React.FC = () => {
-
-  const materialEditViewModel = new MaterialEditViewModel();
-
   return (
     <>
       <Row justify="center">
         <Title>Edite o seu gasto</Title>
       </Row>
 
-      <MaterialForm materialViewModel={materialEditViewModel} />
+      <MaterialForm />
     </>
   );
 };
