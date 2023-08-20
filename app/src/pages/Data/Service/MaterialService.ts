69782601443d4d06b603a7dd4a349d0aa3cbf4f4
Ieleny <ielenyfilgueira@hotmail.com>
2023-08-20 18:03:36 -0300

    feature: create SaveMaterialService

---
+++ app/src/pages/Data/Service/MaterialService.ts
@@ -0,0 +1,39 @@
+import { MaterialModel } from "src/pages/Data/Model/MaterialModel";
+import { MaterialController } from "src/pages/Data/Store/MaterialController";
+import { IMaterialSave } from "src/interface/IMaterialSave";
+
+export default class SaveMaterialService {
+  private materialModel = new MaterialModel();
+  private materialController = new MaterialController();
+
+  public save = ({
+    key,
+    productName,
+    quantityBought,
+    priceUnd,
+  }: IMaterialSave) => {
+    
+    if (key !== undefined) {
+      this.edit({ key, productName, quantityBought, priceUnd });
+    } else {
+      this.create({ productName, quantityBought, priceUnd });
+    }
+  };
+
+  public create = ({
+    productName,
+    quantityBought,
+    priceUnd,
+  }: IMaterialSave) => {
+    const countMaterial = this.materialModel.amountMaterial();
+    this.materialController.addMaterial({
+      materiais: { key: countMaterial, quantityBought, productName, priceUnd },
+    });
+  };
+
+  public edit = ({ key, productName, quantityBought, priceUnd }: IMaterialSave) => {
+    this.materialController.editMaterial({
+      materiais: { key: key ?? 0, quantityBought, productName, priceUnd },
+    });
+  };
+}
